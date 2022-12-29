8a57327113023d10c2a1ad03896b8ac908f251d0
Abhishek Singh Maurya <119608522+abhishek8953328579@users.noreply.github.com>
2022-12-29 19:28:37 +0530

    Update ch9ex2.py

--- ch9ex2.py
+++ ch9ex2.py
@@ -1,5 +1,5 @@
 def finding_num(list1):
     m_list=[str(i) for i in list1 if (type(i)==int or type(i)==float)]
     return m_list
-a=["kaluu",1,2,4,1.02,[1,2,3],"kamleshg"]
+a=["Abhishek",1,2,4,1.02,[1,2,3],"Aman"]
 print(finding_num(a))
