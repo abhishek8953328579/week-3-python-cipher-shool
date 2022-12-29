2b296f34d2d2b72c1a2b86721f343b92d08d8d4f
Abhishek Singh Maurya <119608522+abhishek8953328579@users.noreply.github.com>
2022-12-29 19:53:50 +0530

    Update setsmethod.py

--- setsmethod.py
+++ setsmethod.py
@@ -1,14 +1,14 @@
 # to add
-set1={"satish","ramesh","kamlesh","raju","baburao"}
-set1.add("kallu")
+set1={"Abhishek","aman","medhansh","navlesh","sonu"}
+set1.add("medhansh")
 print(set1)
 # to remove
-set1.remove("kallu")
+set1.remove("medhansh")
 print(set1)
 # to descard agar element present nahi in sets then it displays None
-set1.discard("satish")
+set1.discard("Abhishek")
 print(set1)
-set1.discard("kallu")
+set1.discard("medhansh")
 print(set1)
 # we can also use copy method
 # also clear method just like list
